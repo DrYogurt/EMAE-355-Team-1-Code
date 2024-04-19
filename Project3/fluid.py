8156e2008f614a098672541bdbae17acd7c07076
DrYogurt <ben.z.rubinstein@gmail.com>
2024-04-19 17:13:32 -0400

    added trivial values to fluid, made pipe calculations work
    Co-authored-by: Tyler <tyler-griffith@users.noreply.github.com>
    Co-authored-by: Jaffer <jaffer-ali@users.noreply.github.com>

--- Project3/fluid.py
+++ Project3/fluid.py
@@ -16,11 +16,21 @@ class Fluid():
             "cvmass": "Cvmass",
             "sound_speed": "A",
             "viscosity": "V",
-            "prandtl": "Prandtl"
+            "prandtl": "Prandtl",
+        }
+        
+        self.trivial_properties = {
+            "critical_pressure": "P_critical",
+            "critical_temperature": "T_critical",
+            "triple_pressure": "p_triple",
+            "triple_temperature": "T_triple",
+            "molar_mass": "molar_mass",
+            "molar_mass": "molar_mass",
+            "gas_constant": "gas_constant",
+            "accentric_factor": "acentric_factor",
         }
     
     def __getattr__(self, property_name):
-
         if property_name in self.properties_dict.keys():
             def property_method(**kwargs):
                 if len(kwargs) != 2:
@@ -35,5 +45,15 @@ class Fluid():
                     raise ValueError(f"Error in calculating property: {e}")
 
             return property_method
-
+        
+        if property_name in self.trivial_properties.keys():
+            def property_method():
+                try:
+                    property = CP.PropsSI(self.trivial_properties[property_name],"",0,"",0, self.name)
+                    return property
+                except Exception as e:
+                    raise ValueError(f"Error in calculating property: {e}")
+            return property_method
+                
+            
         raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{property_name}'")
